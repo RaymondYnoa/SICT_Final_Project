4480708ce043f841c4c95b74cf700b8eeca999bc
Raymond <ynoa.raymond@gmail.com>
2023-12-07 08:16:52 -0500

    Database updated. Code is functional with SQL database connected through MySQL.

--- src/main/java/com/company/employeemanagementsystem/repository/EmployeeRepository.java
+++ src/main/java/com/company/employeemanagementsystem/repository/EmployeeRepository.java
@@ -1,9 +1,9 @@
-// src/main/java/com/company/employeemanagementsystem/repository/EmployeeRepository.java
-
 package com.company.employeemanagementsystem.repository;
 
 import com.company.employeemanagementsystem.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 }
